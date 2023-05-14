4411ed4c6e8ba10de191fa409c1c42ca01fc6c61
mrzogy <magbolmrzogy@gmail.com>
2023-05-14 17:02:39 +0300

    e

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+/*:
+ ### Overview:
+ In this assignment, you will be exploring higher-order functions in Swift, which are functions that take other functions as arguments or return functions as output. You will implement several common higher-order functions and use them to solve practical problems.
+
+ ### Requirements:
+
+ Implement the following higher-order functions in Swift:
+ - Map
+ - Filter
+ - Reduce
+ 
+ In this assignment, you will use the Book struct provided below to practice using higher-order functions in Swift.
+ */
+struct Book {
+    var title: String
+    var author: String
+    var price: Double
+    var nPage: Int
+    var readingAge: Int
+}
+/*:
+ You will be working with an array of Book instances:
+ */
+//let allbooks = [
+
+//    ]
+/*:
+ - map(): Use the map() function to create a new array of book titles with author.
+ - filter(): Use the filter() function to create a new array of books with reading age of 16 or higher.
+ - reduce(): Use the reduce() function to find the total cost of all books in the array.
+ - contains(): Use the contains() function to check if the array of books contains a specific book.
+ */
